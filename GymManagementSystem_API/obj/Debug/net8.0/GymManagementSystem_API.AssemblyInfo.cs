4d8b0e18fdd048cf867cfc0815db2e954c4f1c70
burakgnydin <brkgnydn9@gmail.com>
2025-05-03 18:25:13 +0300

    son

--- GymManagementSystem_API/obj/Debug/net8.0/GymManagementSystem_API.AssemblyInfo.cs
+++ GymManagementSystem_API/obj/Debug/net8.0/GymManagementSystem_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymManagementSystem_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10d73149687da1dc267af40914490992495653c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fea2c9e62fe7315b726d87d13242bdd71b741125")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymManagementSystem_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymManagementSystem_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
