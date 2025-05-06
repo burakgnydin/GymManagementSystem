0b4d17903acdba23fe2d5d6eb96e9c6f1af0db37
AhmetDemiir <ahmet46.46demir@gmail.com>
2025-05-06 21:12:58 +0300

    Yerel değişiklikler kaydedildi

--- GymManagementSystem_API/obj/Debug/net8.0/GymManagementSystem_API.AssemblyInfo.cs
+++ GymManagementSystem_API/obj/Debug/net8.0/GymManagementSystem_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymManagementSystem_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+892e280be7e3a09230c15b9847e2d92308660e16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymManagementSystem_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymManagementSystem_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
